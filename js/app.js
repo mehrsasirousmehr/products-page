ec93103bbf5a2665a939e1e446ad68919ce7e294
Mehrsa <mhrs.project22@gmail.com>
2023-12-23 17:09:24 +0330

    search products name

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".product-item");
+
+// search products name
+const searchHandler = (event) => {
+    const searchValue = event.target.value.toLowerCase().trim();
+    products.forEach((product) => {
+        const productName = product.children[1].innerText.toLowerCase();
+        console.log(productName);
+
+        if (productName.includes(searchValue)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+};
+
+searchInput.addEventListener("keyup", searchHandler);
